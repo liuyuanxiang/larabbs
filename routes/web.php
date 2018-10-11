6ee9c7d5021d38d6448a0968f0c7852cd592cf63
YashonLvan <503931578@qq.com>
2018-10-11 15:33:23 +0800

    generated topics

--- routes/web.php
+++ routes/web.php
@@ -35,3 +35,5 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 // Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
