0473eb5cc23859a69fbf08fa9e1f018ca6ddc765
YashonLvan <503931578@qq.com>
2018-10-15 09:37:28 +0800

    Notify author when topic replied

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -16,6 +16,9 @@ class NotificationsController extends Controller
     {
         // 获取登录用户的所有通知
         $notifications = Auth::user()->notifications()->paginate(20);
+        // 标记为已读，未读数量清零
+        Auth::user()->markAsRead();
+        
         return view('notifications.index', compact('notifications'));
     }
 }
