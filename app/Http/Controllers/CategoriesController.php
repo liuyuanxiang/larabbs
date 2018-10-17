6cb0e9dab793cdeb40146f13dcc21fe3609b8719
YashonLvan <503931578@qq.com>
2018-10-17 16:57:32 +0800

    Sidebar links

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -6,16 +6,18 @@ use Illuminate\Http\Request;
 use App\Models\Topic;
 use App\Models\Category;
 use App\Models\User;
+use App\Models\Link;
 
 class CategoriesController extends Controller
 {
-    public function show(Category $category, Request $request, Topic $topic,User $user)
+    public function show(Category $category, Request $request, Topic $topic,User $user,Link $link)
     {
         // 读取分类 ID 关联的话题，并按每 20 条分页
         $topics = $topic->withOrder($request->order)->where('category_id', $category->id)->paginate(20);
         //从缓存中取前6位活跃用户
         $active_users = $user->getActiveUsers();
         // 传参变量话题和分类到模板中
-        return view('topics.index', compact('topics', 'category','active_users'));
+        $links = $link->getAllCached();
+        return view('topics.index', compact('topics', 'category','active_users','links'));
     }
 }
